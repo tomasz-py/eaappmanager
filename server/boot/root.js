e1ef6841bb83c4b5f804556a7a259a95f5d6ea04
Tomasz-py <tomasz.pyclik@gmail.com>
2019-03-08 12:44:59 +0100

    Add AppUser model

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,11 +1,11 @@
 "use strict";
 
-//module.exports = async function(server) {
-module.exports = function(server) {
+module.exports = async function(server) {
+  //module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get("/", server.loopback.status());
   server.use(router);
-  //const datasource = server.datasources.eaappmanagerdb;
-  //await datasource.autoupdate();
+  const datasource = server.datasources.eaappmanagerdb;
+  await datasource.autoupdate().catch(err => console.log(err));
 };

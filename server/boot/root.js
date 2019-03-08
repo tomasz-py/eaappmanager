0c7602248a3737271231d699f0c6d342a006565b
Tomasz-py <tomasz.pyclik@gmail.com>
2019-03-08 09:11:34 +0100

    Datasource change

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,11 @@
-'use strict';
+"use strict";
 
+//module.exports = async function(server) {
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get("/", server.loopback.status());
   server.use(router);
+  //const datasource = server.datasources.eaappmanagerdb;
+  //await datasource.autoupdate();
 };

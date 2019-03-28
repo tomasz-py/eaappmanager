d3de9a811170e2a7618eadad53e1f9b035a0734a
Tomasz-py <tomasz.pyclik@gmail.com>
2019-03-28 17:33:11 +0100

    Add basic ACLs + create admin role

---
+++ server/boot/sample-models.js
@@ -0,0 +1,26 @@
+module.exports = function(app) {
+  //var User = app.models.AppUser;
+  //   var Role = app.models.Role;
+  //   var RoleMapping = app.models.RoleMapping;
+  //   //create the admin role
+  //   Role.create(
+  //     {
+  //       name: "admin"
+  //     },
+  //     function(err, role) {
+  //       if (err) throw err;
+  //       console.log("Created role:", role);
+  //       //make admin an admin
+  //       role.principals.create(
+  //         {
+  //           principalType: RoleMapping.USER,
+  //           principalId: 1
+  //         },
+  //         function(err, principal) {
+  //           if (err) throw err;
+  //           console.log("Created principal:", principal);
+  //         }
+  //       );
+  //     }
+  //   );
+};

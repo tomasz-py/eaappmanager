a8ef3b3bd965eab0e5dd6d43988e9441ccb0cdd4
Tomasz-py <tomasz.pyclik@gmail.com>
2019-04-04 19:08:51 +0200

    Add toRestart remote  method

--- common/models/instance.js
+++ common/models/instance.js
@@ -1,5 +1,31 @@
-'use strict';
+"use strict";
 
 module.exports = function(Instance) {
-
+  Instance.toRestart = function(id, cb) {
+    Instance.find(
+      {
+        where: { id: id },
+        fields: {
+          description: false
+        },
+        include: [
+          {
+            relation: "service",
+            scope: {
+              fields: { id: true },
+              where: { toRestart: true, statusId: 1 }
+            }
+          }
+        ]
+      },
+      cb
+    );
+  };
+  Instance.remoteMethod("toRestart", {
+    accepts: { arg: "id", type: "number", required: true },
+    returns: { arg: "instance", type: "array" },
+    description:
+      "Return id's of services to restart (where toRestart is True and status is Online)",
+    http: { path: "/services-torestart", verb: "get" }
+  });
 };

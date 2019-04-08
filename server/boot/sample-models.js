c88dd1f111f247738d930840538687b81811678b
Tomasz-py <tomasz.pyclik@gmail.com>
2019-04-08 15:43:31 +0200

    Add create user and model if users not exist

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,26 +1,40 @@
-module.exports = function(app) {
-  //var User = app.models.AppUser;
-  //   var Role = app.models.Role;
-  //   var RoleMapping = app.models.RoleMapping;
-  //   //create the admin role
-  //   Role.create(
-  //     {
-  //       name: "admin"
-  //     },
-  //     function(err, role) {
-  //       if (err) throw err;
-  //       console.log("Created role:", role);
-  //       //make admin an admin
-  //       role.principals.create(
-  //         {
-  //           principalType: RoleMapping.USER,
-  //           principalId: 1
-  //         },
-  //         function(err, principal) {
-  //           if (err) throw err;
-  //           console.log("Created principal:", principal);
-  //         }
-  //       );
-  //     }
-  //   );
+module.exports = async function(app) {
+  const User = app.models.AppUser;
+  const Role = app.models.Role;
+  const RoleMapping = app.models.RoleMapping;
+
+  const currentUsersCount = await User.count();
+  if (currentUsersCount > 0) {
+    console.log("Users already exist");
+    return;
+  }
+
+  User.create(
+    { username: "admin", email: "admin@admin.pl", password: "admin!?" },
+    function(err, userInstance) {
+      console.log(userInstance);
+    }
+  );
+
+  //create the admin role
+  Role.create(
+    {
+      name: "admin"
+    },
+    function(err, role) {
+      if (err) throw err;
+      console.log("Created role:", role);
+      //make admin an admin
+      role.principals.create(
+        {
+          principalType: RoleMapping.USER,
+          principalId: 1
+        },
+        function(err, principal) {
+          if (err) throw err;
+          console.log("Created principal:", principal);
+        }
+      );
+    }
+  );
 };

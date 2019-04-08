614dfd21b1f869c6420c40691b895ed27d5b382b
Tomasz-py <tomasz.pyclik@gmail.com>
2019-04-08 20:40:26 +0200

    Add users and role seed

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -10,29 +10,58 @@ module.exports = async function(app) {
   }
 
   User.create(
-    { username: "admin", email: "admin@admin.pl", password: "admin!?" },
-    function(err, userInstance) {
-      console.log(userInstance);
-    }
-  );
-
-  //create the admin role
-  Role.create(
-    {
-      name: "admin"
-    },
-    function(err, role) {
+    [
+      { username: "admin", email: "admin@admin.pl", password: "admin!?" },
+      { username: "test", email: "test@test.pl", password: "test" }
+    ],
+    function(err, users) {
       if (err) throw err;
-      console.log("Created role:", role);
-      //make admin an admin
-      role.principals.create(
+
+      console.log("Created users:", users);
+
+      //create the admin role
+      Role.create(
         {
-          principalType: RoleMapping.USER,
-          principalId: 1
+          name: "admin"
         },
-        function(err, principal) {
+        function(err, role) {
           if (err) throw err;
-          console.log("Created principal:", principal);
+          console.log("Created role:", role);
+          //make admin an admin
+          role.principals.create(
+            {
+              principalType: RoleMapping.USER,
+              principalId: users[0].id
+            },
+            function(err, principal) {
+              if (err) throw err;
+
+              console.log("Created principal:", principal);
+            }
+          );
+        }
+      );
+
+      //create the user role
+      Role.create(
+        {
+          name: "user"
+        },
+        function(err, role) {
+          if (err) throw err;
+          console.log("Created role:", role);
+          //make test an user
+          role.principals.create(
+            {
+              principalType: RoleMapping.USER,
+              principalId: users[1].id
+            },
+            function(err, principal) {
+              if (err) throw err;
+
+              console.log("Created principal:", principal);
+            }
+          );
         }
       );
     }

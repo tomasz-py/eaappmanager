44de9d5a4a632a6355416ed5e84180204f7c0932
Tomasz-py <tomasz.pyclik@gmail.com>
2019-04-16 12:14:45 +0200

    Add to queue after save Instance

--- common/models/instance.js
+++ common/models/instance.js
@@ -1,6 +1,9 @@
 "use strict";
 
 module.exports = function(Instance) {
+  var app = require("../../server/server");
+
+  //Get service id's to restart (status online and toRestart flag on true)
   Instance.toRestart = function(id, cb) {
     Instance.find(
       {
@@ -32,5 +35,38 @@ module.exports = function(Instance) {
     description:
       "Return id's of services to restart (where toRestart is True and status is Online)",
     http: { path: "/services-torestart/:id", verb: "get" }
+  });
+
+  //Add information to queue after save or create instance
+  Instance.observe("after save", function(ctx, next) {
+    const Queue = app.models.Queue;
+    //If new instance: add to queue information about create instance
+    if (ctx.isNewInstance) {
+      Queue.create(
+        {
+          tableName: "instance",
+          itemId: ctx.instance.id,
+          action: "Create",
+          statusId: 3
+        },
+        function(err) {
+          if (err) throw err;
+        }
+      );
+    } else {
+      //If update instance: add to queue information about update instance
+      Queue.create(
+        {
+          tableName: "instance",
+          itemId: ctx.instance.id,
+          action: "Update",
+          statusId: 3
+        },
+        function(err) {
+          if (err) throw err;
+        }
+      );
+    }
+    next();
   });
 };
